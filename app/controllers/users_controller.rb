fcc23ee69a105a2542b3f14e06a7a2530c165bba
austinjuliuskim <austinjuliuskim@gmail.com>
2016-09-29 01:24:13 -0700

    Add Sign Up and flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
 
   def show
-    
+    @user = User.find(params[:id])
   end
 
   def new
-
+    @user = User.new()
   end
 
   def create
-
+    @user = User.new
 
     if @user.save
-
+      flash[:success] = "Welcome to the Stone Daddies!"
+      redirect_to root_path
     else
-
+      render 'new'
     end
   end
 
@@ -25,7 +26,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post '/contacts', to: 'contacts#create'
   get '/contacts', to: 'contacts#index'
 
-  
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

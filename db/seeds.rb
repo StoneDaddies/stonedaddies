a0df7e03868c6bd4e5edbbbae965cfcd8b1c3b93
austinjuliuskim <austinjuliuskim@gmail.com>
2016-09-30 00:30:38 -0700

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# User.where(email: "stonedaddies@gmail.com", password: "Stonedaddies4me", role: "Admin").first_or_create
-# User.where(email: "austinjuliuskim@gmail.com", password: "Stonedaddies4you", role: "Admin").first_or_create
+User.where(email: "stonedaddies@gmail.com", password: "Stonedaddies4me", role: "Admin").first_or_create
+User.where(email: "austinjuliuskim@gmail.com", password: "Stonedaddies4you", role: "Admin").first_or_create

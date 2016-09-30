886ce9a675a7dbd801764d7014116b59597b9995
austinjuliuskim <austinjuliuskim@gmail.com>
2016-09-30 00:46:36 -0700

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-User.where(email: "stonedaddies@gmail.com", password: "Stonedaddies4me", password_confirmation: "Stonedaddies4me" role: "Admin").first_or_create
-User.where(email: "austinjuliuskim@gmail.com", password: "Stonedaddies4you",password_confirmation: "Stonedaddies4you", role: "Admin").first_or_create
+User.where(email: "stonedaddies@gmail.com", password: "Stonedaddies4me", password_confirmation: "Stonedaddies4me", role: "Admin").first_or_create
+User.where(email: "austinjuliuskim@gmail.com", password: "Stonedaddies4you", password_confirmation: "Stonedaddies4you", role: "Admin").first_or_create
